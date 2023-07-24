3aec64ff44ad016795f5fb656d8b90397061bc6c
Fatih Sarikaya <fatihsarikaya4@hotmail.com>
2023-07-24 17:48:04 +0300

    Create Daire_Alani_Hesaplama.java

---
+++ Daire_Alani_Hesaplama.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Daire_Alani_Hesaplama {
+    public static void main(String [] args){
+        int r,a;
+        double pi=3.14;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen Dairenin Yarı Çapını Giriniz : ");
+        r = input.nextInt();
+        System.out.print("Lütfen Merkez Açısını Giriniz : ");
+        a = input.nextInt();
+
+        double alan = pi * r * r ;
+        double cevre = 2 * pi * r;
+        double dairedilimi = ((alan * a) / 360);
+
+        System.out.println("Dairenizin Alanı : " + alan);
+        System.out.println("Dairenizin Çevresi : " + cevre);
+        System.out.println("Dairenizin Dilimi Alanı : " + dairedilimi);
+
+    }
+}
